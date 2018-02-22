833ef5415c6f58d6c4a3a42374d3b20d9770d329
angcyo <angcyo@126.com>
2018-02-22 17:21:24 +0800

    init

---
+++ app/src/main/java/com/student/manager/App.kt
@@ -0,0 +1,20 @@
+package com.student.manager
+
+import com.angcyo.uiview.RApplication
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2018/02/22 15:49
+ * 修改人员：Robi
+ * 修改时间：2018/02/22 15:49
+ * 修改备注：
+ * Version: 1.0.0
+ */
+class App : RApplication() {
+    override fun onInit() {
+        super.onInit()
+    }
+}
